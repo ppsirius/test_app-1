a05a7626bcc148ab53d3e4491397e661421205c7
Piotr Pupczyk <piotrek@jakbyco.com>
2014-10-26 11:33:41 +0100

    last commit :(

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -14,38 +14,45 @@ class PostsControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    sign_in users(:one)
     get :new
     assert_response :success
   end
 
   test "should create post" do
+    sign_in users(:one)
     assert_difference('Post.count') do
-      post :create, post: { body: @post.body, title: @post.title, user_id: @post.user_id }
+      post :create, post: { body: 'tresc', title: 'tytul', user_id: 2 }
     end
 
     assert_redirected_to post_path(assigns(:post))
   end
 
   test "should show post" do
+    sign_in users(:one)
     get :show, id: @post
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in users(:one)
     get :edit, id: @post
     assert_response :success
   end
 
   test "should update post" do
+    sign_in users(:one)
     patch :update, id: @post, post: { body: @post.body, title: @post.title, user_id: @post.user_id, visible: @post.visible }
     assert_redirected_to post_path(assigns(:post))
   end
 
   test "should destroy post" do
+    sign_in users(:one)
     assert_difference('Post.count', -1) do
-      delete :destroy, id: @post
+      delete :destroy, :id => posts(:one).to_param
     end
 
     assert_redirected_to posts_path
   end
 end
+
